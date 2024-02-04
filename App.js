a7288c9bb3cf03de9c0177ec0f2609bab7dcbd55
iko2000 <123463655+iko2000@users.noreply.github.com>
2024-02-04 14:55:55 +0400

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
+import { colors } from "./src/utiles/colors";
+import Focus from "./src/features/focus";
+import { useState } from "react";
+import Countdown from "./src/components/countdown";
+import { useStorgecomponent } from './src/store/store';
+
 
 export default function App() {
+  const focusedSubject = useStorgecomponent((state) => state.currentTopic);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+     {focusedSubject ? <Countdown/> : <Focus/> }
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    backgroundColor: colors.darkBlue,
+  },
+  text: {
+    color: colors.white,
   },
 });
